3bb813023437b97e3785fe3d306828ed08036bf8
bryanfong.nyc@gmail.com <bryanfong.nyc@gmail.com>
2018-07-10 02:02:49 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,7 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 WIN_COMBINATIONS = [ 
-  [0,1,2], # Top row
+  [0,1,1], # Top row
   [3,4,5], # Middle row
   [6,7,8], # Bottom row
   [0,3,6], # Left column
